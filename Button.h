795dd5b5126331cc7118d29250a7c3107f54da0b
DugnCon <150952695+DugnCon@users.noreply.github.com>
2024-03-25 15:08:07 +0700

    Button.h

--- Button.h
+++ Button.h
@@ -3,18 +3,24 @@
 #include <SDL_ttf.h>
 #include <SDL_image.h>
 #include <iostream>
+#include "Mixer.h"
+#include "MixerTwo.h"
 class Button
 {
 private:
 	SDL_Renderer* renderer = nullptr;
 	SDL_Texture* texture = nullptr;
-	//SDL_Texture* texture1 = nullptr; //Dung de chua be noi chu va mau cho texture
 	SDL_Color textColor = { 255,255,255,255 };
 	TTF_Font* font = nullptr;
 	//SDL_Color holerColor = { 255,0,0,0 };
 	std::string content;
+	SDL_Surface* surface = nullptr;
 	SDL_Rect des;
 	SDL_Color Color = { 255,255,255,255 };//white
+	MixerTwo* chuck = nullptr;
+	MixerTwo* chuckTwo = nullptr;
+	Mixer* music = nullptr;
+	bool isPressed = true;
 public:
 	Button(SDL_Renderer* renderer, TTF_Font* font, SDL_Point point, std::string content) {
 		this->renderer = renderer;
@@ -27,5 +33,11 @@ public:
 	void render();
 	void renderText();
 	bool handleEvents(SDL_Event e);
+	//bool handleEventsMusic(SDL_Event e);
+	void Chuck();
+	void PlayChuck();
+	void PlayChuckTwo();
+	void Stop();
+	void Volume();
 };
 
